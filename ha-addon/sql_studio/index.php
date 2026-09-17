af5da84dce8c67e6e4e073da40948f4195f760a8
jeremiahjp <jeremiahjp@users.noreply.github.com>
2026-09-17 10:14:18 -0500

    fix(sql_studio): fix PHP parse error in index.php and bump to 1.0.2

--- ha-addon/sql_studio/index.php
+++ ha-addon/sql_studio/index.php
@@ -1,9 +1,13 @@
 <?php
 // SQL Studio - Adminer 6.1.0
-if (empty() && empty()) {
-    ['pgsql'] = '192.168.68.84:5432';
-    ['username'] = 'postgres';
-    ['db'] = 'emporia_energy';
+if (!isset($_GET['pgsql'])) {
+    $_GET['pgsql'] = '192.168.68.84:5432';
+}
+if (!isset($_GET['username'])) {
+    $_GET['username'] = 'postgres';
+}
+if (!isset($_GET['db'])) {
+    $_GET['db'] = 'emporia_energy';
 }
 
 require_once __DIR__ . '/adminer.php';
